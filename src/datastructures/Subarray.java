ed593d9aaa3a21c2c77a022cab61ac38b1bf225c
Odiaz Bumma <odiazbumma@gmail.com>
2019-11-04 21:01:59 +0700

    Data structures: subarray, done

---
+++ src/datastructures/Subarray.java
@@ -0,0 +1,30 @@
+package datastructures;
+
+import java.util.Scanner;
+
+public class Subarray {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+            array[i] = scanner.nextInt();
+        int count = 0;
+        for (int i = 0 ; i <= n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                int temp = 0;
+                for (int k = 0; k <= i; k++) {
+                    if (k+j < n) {
+                        temp += array[k + j];
+                        //System.out.print(array[k+j] + " + ");
+                    }
+                }
+                if (temp < 0)
+                    count++;
+                //System.out.println(" = " + temp);
+            }
+        }
+        System.out.println(count);
+    }
+}
