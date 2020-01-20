2fe592e9af588ab697fb466b0271d91aad8e34b6
Odiaz Bumma <odiazbumma@gmail.com>
2020-01-20 11:30:22 +0700

    Added Introduction: Java Loops I

---
+++ src/introduction/LoopsI.java
@@ -0,0 +1,20 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class LoopsI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        for (int i=1;i<=10;i++) {
+            int temp = N*i;
+            System.out.println(N + " x " + i + " = " + temp);
+        }
+    }
+}
