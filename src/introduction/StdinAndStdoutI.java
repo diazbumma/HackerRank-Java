329deb63239983762ba11782f8ebe498c2165906
Odiaz Bumma <odiazbumma@gmail.com>
2020-01-20 11:18:09 +0700

    Added Introduction: Stdin and Stdout I

---
+++ src/introduction/StdinAndStdoutI.java
@@ -0,0 +1,20 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class StdinAndStdoutI {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
