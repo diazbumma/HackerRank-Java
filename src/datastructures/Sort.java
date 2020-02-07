d344090b78443f9fb216e1948a9847eb8ba3ced0
Odiaz Bumma <odiazbumma@gmail.com>
2020-02-07 17:51:16 +0700

    Change student to static inner class

--- src/datastructures/Sort.java
+++ src/datastructures/Sort.java
@@ -5,45 +5,45 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-class Student implements Comparable<Student> {
-
-    private int id;
-    private String fname;
-    private double cgpa;
-
-    public Student(int id, String fname, double cgpa) {
-        super();
-        this.id = id;
-        this.fname = fname;
-        this.cgpa = cgpa;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public double getCgpa() {
-        return cgpa;
-    }
-
-    @Override
-    public int compareTo(Student o) {
-        int compare;
-        if (cgpa != o.getCgpa())
-            compare = (cgpa<o.getCgpa() ? 1 : -1);
-        else if (!fname.equals(o.getFname()))
-            compare = (fname.compareTo(o.getFname()));
-        else
-            compare = (id<o.getId() ? -1 : 1);
-        return compare;
-    }
-}
-
 public class Sort {
+
+    static class Student implements Comparable<Student> {
+
+        private int id;
+        private String fname;
+        private double cgpa;
+
+        public Student(int id, String fname, double cgpa) {
+            super();
+            this.id = id;
+            this.fname = fname;
+            this.cgpa = cgpa;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getFname() {
+            return fname;
+        }
+
+        public double getCgpa() {
+            return cgpa;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            int compare;
+            if (cgpa != o.getCgpa())
+                compare = (cgpa<o.getCgpa() ? 1 : -1);
+            else if (!fname.equals(o.getFname()))
+                compare = (fname.compareTo(o.getFname()));
+            else
+                compare = (id<o.getId() ? -1 : 1);
+            return compare;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
