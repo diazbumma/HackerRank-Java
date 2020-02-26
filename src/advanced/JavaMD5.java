5b0d1cab218b2a72cf7f5f3395b1c26d4a16e938
Odiaz Bumma <odiazbumma@gmail.com>
2020-02-26 14:44:04 +0700

    Advanced: Java MD5, done

---
+++ src/advanced/JavaMD5.java
@@ -0,0 +1,37 @@
+package advanced;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class JavaMD5 {
+
+    public static String getMd5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            String hashtext = no.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String str = scanner.nextLine();
+
+        System.out.println(getMd5(str));
+    }
+}
